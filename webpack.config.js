01a3a49fd7b23bfda948e39775766f32464349b8
Kalle Wallin <kalle.wallin@gmail.com>
2016-07-07 20:03:22 +0200

    setup hot module replacement

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
     app: path.join(__dirname, 'app'),
     build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
     entry: {
         app: PATHS.app
     },
@@ -14,3 +17,25 @@ module.exports = {
         filename: 'bundle.js'
     }
 };
+
+if(TARGET === 'build'){
+    module.exports = merge(common, {});
+}
+if(TARGET === 'start' || !TARGET){
+    // start or default
+    module.exports = merge(common, {
+        devServer: {
+            contentBase: PATHS.build,
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
+            stats: 'errors-only',
+            host: process.env.HOST,
+            port: process.env.PORT
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    });
+}
